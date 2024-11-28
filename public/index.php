97199084e3b49b77704aa7056a2ff38c1258525a
Mai Trần Tuấn Kiệt <117163756+mttk2004@users.noreply.github.com>
2024-11-28 09:43:43 +0700

    Initial commit

---
+++ public/index.php
@@ -0,0 +1,45 @@
+<?php
+
+use Core\Session;
+use Core\ValidationException;
+
+
+// Start session
+session_start();
+
+// Root path of the project
+const BASE_PATH = __DIR__ . '/../';
+
+// Autoload
+require_once BASE_PATH . './vendor/autoload.php';
+
+// Load helper functions
+require_once BASE_PATH . './utils/helperFunctions.php';
+
+// Bootstrap the application
+require_once BASE_PATH . './bootstrap.php';
+
+// Routes
+require_once BASE_PATH . './routes.php';
+
+// Get the URI and method of the request
+$uri = parse_url($_SERVER['REQUEST_URI'])['path'];
+$method = $_POST['_method'] ?? $_SERVER['REQUEST_METHOD'];
+
+// Route the request
+try {
+	$router->route($uri, $method);
+} catch (ValidationException $e) {
+	// Set the session for error
+	Session::flash('errors', $e->errors);
+	Session::flash('old', $e->old);
+	
+	// Redirect back to the previous URL
+	redirect($router->previousUrl());
+} catch (Exception $e) {
+	// Redirect to 404 page if no route is found
+	abort();
+}
+
+// End flash session
+Session::unFlash();
